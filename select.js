5b16a22c384ab5591a30b3941f9020b90f53672b
Sachini1990 <Sachini1990@users.noreply.github.com>
2015-03-23 14:41:00 +0530

    Create select.js

---
+++ select.js
@@ -0,0 +1,12 @@
+
+
+$("#select1").change(function() { 
+if($(this).data('options') == undefined){
+    /*Taking an array of all options-2 and kind of embedding it on the select1*/
+    $(this).data('options',$('#select2 option').clone());
+    } 
+var id = $(this).val();
+    //'[value*="' + $(this).val()  + '"]'
+var options = $(this).data('options').filter('[value*="' + id + '-"]');
+$('#select2').html(options);
+});
