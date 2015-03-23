d50c3f11b57a8072904d502ddb793fa7e0820212
Sachini1990 <Sachini1990@users.noreply.github.com>
2015-03-23 14:53:56 +0530

    Update select.js

--- select.js
+++ select.js
@@ -1,12 +1,10 @@
-
-
 $("#select1").change(function() { 
-if($(this).data('options') == undefined){
+    $('#select2').prop('disabled', false);
+	if($(this).data('options') == undefined){
     /*Taking an array of all options-2 and kind of embedding it on the select1*/
     $(this).data('options',$('#select2 option').clone());
     } 
-var id = $(this).val();
-    //'[value*="' + $(this).val()  + '"]'
-var options = $(this).data('options').filter('[value*="' + id + '-"]');
-$('#select2').html(options);
+	var id = $(this).val();
+	var options = $(this).data('options').filter('[value*="' + id + '-"]');
+	$('#select2').html(options);
 });
